3e5bc2927d110bad7a9e4d09edfbdba02433ad2a
Hai Bui <haibui19872015@gmail.com>
2015-09-03 10:39:01 +0700

    [ADD] local notify demo 1. ask for permission 2. schedule - optionally, clear all local notifs

--- Learn Notification/Learn Notification/ViewController.swift
+++ Learn Notification/Learn Notification/ViewController.swift
@@ -19,7 +19,54 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    @IBAction func registerNotifTypes(sender: AnyObject) {
+        
+        
+        let userNotifTypes = UIUserNotificationType(rawValue: UIUserNotificationType.Alert.rawValue | UIUserNotificationType.Sound.rawValue | UIUserNotificationType.Badge.rawValue)
+        
+        
+        let settings = UIUserNotificationSettings(forTypes: userNotifTypes, categories: nil)
+        
+        UIApplication.sharedApplication().registerUserNotificationSettings(settings)
+        
+        
+        
+        
+        
+    }
+    
+    @IBAction func clearScheduledNotifs(sender: AnyObject) {
+        
+        UIApplication.sharedApplication().cancelAllLocalNotifications()
+        
+        
+        
+    }
 
+    @IBAction func setUpLocalNotif(sender: AnyObject) {
+        
+        let notif = UILocalNotification()
+        notif.fireDate = NSDate().dateByAddingTimeInterval(5.0)
+        notif.alertBody = "alert body"
+        notif.alertTitle = "alert title"
+        notif.alertAction = "some action"
+        notif.soundName = UILocalNotificationDefaultSoundName
+        notif.applicationIconBadgeNumber = 1
+        notif.userInfo = ["my key" : "my value",]
+        UIApplication.sharedApplication().scheduleLocalNotification(notif)
+        
+        
+        
+        
+    }
 
+    @IBAction func setUpRemoteNotif(sender: AnyObject) {
+        
+        
+        
+        
+    }
 }
 
