41a43daf5eb28194a05f27d64f897f261892cebb
Hai Bui <haibui19872015@gmail.com>
2015-09-10 17:30:47 +0700

    [ADD] project to learn core animation

---
+++ Core Animation/Core Animation/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Core Animation
+//
+//  Created by SB 8 on 9/10/15.
+//  Copyright © 2015 vn.haibui. All rights reserved.
+//
+
+import UIKit
+import QuartzCore
+
+class ViewController: UIViewController {
+    @IBOutlet weak var layerView: UIView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
